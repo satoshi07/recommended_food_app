d72ccfee4f10c7571976df48dec071a6f9556b25
satoshi07 <satoshi.akaike7@gmail.com>
2021-05-28 01:16:37 +0900

    初期データの投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,18 @@
-User.create!(email: "test@example.com", password: "password")
-puts "ユーザーの初期データインポートに成功しました。"
+email = "test@example.com"
+password = "password"
+
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
+
+user1 = User.create!(email: "satou@example.com", password: "password")
+user2 = User.create!(email: "suzuki@example.com", password: "password")
+user3 = User.create!(email: "tanaka@example.com", password: "password")
+
+user2.foods.create!(name: "沖縄そば")
+user1.foods.create!(name: "海ぶどう")
+user3.foods.create!(name: "ゴーヤーチャンプルー")
+user3.foods.create!(name: "サーターアンダギー")
+user1.foods.create!(name: "タコライス")
+
+User.create!(email: email, password: password)
+
+puts "初期データの投入に成功しました！"
