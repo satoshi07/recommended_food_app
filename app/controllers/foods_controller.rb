e02112c7871132df710c4d816fc09cf8adda200b
satoshi07 <satoshi.akaike7@gmail.com>
2021-05-29 00:23:02 +0900

    詳細表示機能の実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -12,7 +12,9 @@ class FoodsController < ApplicationController
     redirect_to food
   end
 
-  def show; end
+  def show
+    @food = Food.find(params[:id])
+  end
 
   def edit; end
 
@@ -23,6 +25,6 @@ class FoodsController < ApplicationController
   private
 
   def food_params
-    params.require(:food).permit(:name)
+    params.require(:food).permit(:name, :comment)
   end
 end
