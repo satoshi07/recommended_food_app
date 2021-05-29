88ac4162f0dd83ab7549ef262c59960c8a5bc051
satoshi07 <satoshi.akaike7@gmail.com>
2021-05-29 22:38:50 +0900

    「いいね！」状態判定メソッドの作成

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,4 +4,8 @@ class Food < ApplicationRecord
   has_many :liked_users, through: :likes, source: :user
   validates :name, presence: true
   mount_uploader :image, ImageUploader
+
+  def liked_by?(user)
+    likes.exists?(user_id: user.id)
+  end
 end
